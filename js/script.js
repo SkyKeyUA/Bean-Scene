434e159e3a3e1f2013a99546a0498e6484280cdf
OLEKSANDR VEREMIIENKO <120383111+SkyKeyUA@users.noreply.github.com>
2025-04-29 16:16:55 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ function windowLoad() {
     document.addEventListener('click', documentActions);
   }
   html.classList.add('loaded');
+  scrollActions();
+  slidersInit();
 }
 function documentActions(e) {
   const targetElement = e.target;
@@ -16,4 +18,33 @@ function documentActions(e) {
   targetElement.closest('.menu__link') && html.classList.contains('menu-open')
     ? html.classList.remove('menu-open')
     : null;
+}
+function scrollActions() {
+  window.addEventListener('scroll', scrollAction);
+
+  function scrollAction() {
+    // Робота з шапкою
+    const header = document.querySelector('.header');
+    header.classList.toggle('header--scroll', scrollY > 20);
+  }
+}
+function slidersInit() {
+  const sliderListFiveItems = new Swiper('.enjoy__swiper', {
+    slidesPerGroup: 1,
+    slidesPerView: 4,
+    spaceBetween: 18,
+    loop: true,
+    autoHeight: true,
+    breakpoints: {
+      // when window width is >= 480px
+      320: {
+        spaceBetween: 18,
+      },
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 17.333,
+      },
+    },
+  });
 }
